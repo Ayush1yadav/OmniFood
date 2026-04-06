9c86ce59169aab1f9dfa7d2897dd2cf8c72df031
Ayush1yadav <ayushyadav122004@gmail,com>
2026-04-06 13:57:53 +0530

    Done

---
+++ omnifood26/omnifood26/JQuery/app.js
@@ -0,0 +1,14 @@
+// $('h1').css('color','red');
+// $('h1').html("<em>JavaScript</em>");
+// $('h1').text("<em>JavaScript</em>");
+// for(let i = 0; i<document.querySelectorAll('button').length; i++){
+// document.querySelectorAll('button')[i].addEventListener('click',function(){
+//     document.querySelector('h1').innerText ="Javascript";
+// })
+// }
+
+
+$('button').click(function(){
+    $('h1').text("Javascript");
+})
+
